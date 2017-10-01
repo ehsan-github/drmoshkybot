f5b228decae2eac6c942ca0a671cef0a1f853897
Ehsan Heydari <ehsan.heydari.yk@gmail.com>
2017-10-01 19:25:47 +0000

    fixing rows

--- functions.py
+++ functions.py
@@ -66,7 +66,7 @@ def call_handler(bot, update):
     elif type == "menu" :
         text = 'از خدمات زیر انتخاب کنید'
         buttons = [InlineKeyboardButton(text=x["name"], callback_data="service_"+str(index)) for index, x in enumerate(constants.SERVICES)]
-        keyboard = InlineKeyboardMarkup([buttons[0:2],buttons[2:4],buttons[4:6], buttons[6:8]])
+        keyboard = InlineKeyboardMarkup([buttons[0:2],buttons[2:4],buttons[4:6], buttons[6:8], buttons[8:10], buttons[10:12], buttons[12:14], buttons[14:16]])
         bot.editMessageText(chat_id = user_id, message_id = msg_id, text = text, reply_markup = keyboard)
 
     elif type == "children" :
--- services.py
+++ services.py
@@ -15,5 +15,5 @@ def select_service(mybot, user_id):
 
     buttons = [InlineKeyboardButton(text=x["name"], callback_data="service_"+str(index)) for index, x in enumerate(constants.SERVICES)]
 
-    keyboard = InlineKeyboardMarkup([buttons[0:2],buttons[2:4],buttons[4:6], buttons[6:8],buttons[8:10], buttons[10:12]])
+    keyboard = InlineKeyboardMarkup([buttons[0:2],buttons[2:4],buttons[4:6], buttons[6:8],buttons[8:10], buttons[10:12], buttons[12:14], buttons[14:16]])
     mybot.sendMessage(user_id, text = text, reply_markup = keyboard)
